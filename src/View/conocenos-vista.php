49f94756b62f1058d7b07bcfdc89d3b96256962d
Daniela Castro <daniela.castro@phs.mx>
2023-06-21 13:53:35 -0600

    fotografías para galeria + extra

--- src/View/conocenos-vista.php
+++ src/View/conocenos-vista.php
@@ -12,8 +12,8 @@ $jefeDepartamento =  $administrativos->getJefeDepartamento();
     <img class="w-100 img-fluid" src="img/Venus-4.webp" alt="" />
     <div class="position-absolute top-50 start-50 translate-middle w-100">
       <div class="d-flex flex-column justify-content-center align-items-center text-center">
-        <h1 class="fw-bold text-warning">Conócenos</h1>
-        <h1 class="fw-bold text-light d-md-flex d-none">
+        <h1 class="fw-bold text-warning shadow-text">Conócenos</h1>
+        <h1 class="fw-bold text-light d-md-flex d-none shadow-text">
           Ingeniería Mecánica
         </h1>
       </div>
@@ -29,7 +29,7 @@ $jefeDepartamento =  $administrativos->getJefeDepartamento();
 
     <div class="col-lg-6 col-12 p-2 shadow-sm">
       <div class="d-flex justify-content-center align-items-center w-100 h-100">
-        <img class="img-fluid rounded" src="img/IMG_2102.webp" alt="">
+        <img class="img-fluid rounded" src="img/IMG_0474.JPG" alt="">
       </div>
     </div>
 
@@ -154,174 +154,207 @@ $jefeDepartamento =  $administrativos->getJefeDepartamento();
 </section>
 <!--Fin Descripción Coordinador -->
 
-
 <!-- Instalaciones -->
 <div class="bg-primary pt-5"></div>
-<section class="seccionInstalaciones bg-dark">
-  <div class="row px-2 g-0">
-    <div class="col-lg-6 col-12 py-4">
-      <div class="d-flex justify-content-center align-items-center w-100 h-100">
-        <div class="row g-0">
-          <div class="col-12">
-            <h2 class="tituloSeccionOscura text-center font-bold m-3">
-              Instalaciones
-            </h2>
-            <div class="separadorSeccionOscura"></div>
-            <h4 class="text-center text-light fw-bold fs-3">
-              Laboratorios de cómputo
-            </h4>
-          </div>
-          <div class="col-12">
-            <h6 class="text-center text-warning fw-bold fs-6">
-              “El verdadero progreso es el que pone la tecnología al alcance de
-              todos.”<br />
-            </h6>
-
-            <p class="text-light" style="text-align: justify">
-              El departamento de sistemas y computación cuenta con diversos
-              laboratorios de equipo de cómputo en dónde los estudiantes podrán
-              desempañarse en desarrollar diversos programas, aplicaciones y
-              proyectos a lo largo de sus clases prácticas en la carrera de
-              ingeniería en sistemas computacionales. <br /><br />
-              Los laboratorios de cómputo están orientados a servir como
-              herramientas de aprendizaje y apoyo didáctico tanto para los
-              docentes como los alumnos.
-            </p>
-            <div class="justify-content-center text-center">
-              <p><a class="btn-warning w-auto btn font-bold" target="_blank" href="https://bit.ly/3ZrhDeQ">
-                  Reglamento de Laboratorio</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-6 col-12 p-2 d-flex justify-content-center align-items-center">
-      <!-- Galería con modal para laboratorios de cómputo -->
-      <div class="contenedor-galeria bg-dark">
-        <div class="row">
-          <div class="col-lg-6">
-            <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage1">
-              <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_3878.webp" alt="" />
-            </a>
-            <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage2">
-              <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_1110.webp" alt="" />
-            </a>
-          </div>
-
-          <div class="col-lg-6">
-            <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage3">
-              <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_3879.webp" alt="" />
-            </a>
-            <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage4">
-              <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/IMG_0959.webp" alt="" />
-            </a>
-          </div>
-        </div>
-      </div>
-      <!-- Fin Galería con modal para laboratorios de cómputo -->
-    </div>
+<section class="seccionInstalaciones bg-dark p-3">
+  <div class="row p-4 g-0">
+    <h2 class="tituloSeccionOscura text-center font-bold m-3">
+      Instalaciones
+    </h2>
+    <div class="separadorSeccionOscura"></div>
+    <h4 class="text-center text-light fw-bold fs-3">
+      Laboratorios de mecánica
+    </h4>
   </div>
+  <div class="col-12">
+    <h6 class="text-center text-warning fw-bold fs-6">
+      “El verdadero progreso es el que pone la tecnología al alcance de
+      todos.”<br />
+    </h6>
+
+    <p class="text-light" style="text-align: justify">
+      El departamento de sistemas y computación cuenta con diversos
+      laboratorios de equipo de cómputo en dónde los estudiantes podrán
+      desempañarse en desarrollar diversos programas, aplicaciones y
+      proyectos a lo largo de sus clases prácticas en la carrera de
+      ingeniería en sistemas computacionales. <br /><br />
+      Los laboratorios de cómputo están orientados a servir como
+      herramientas de aprendizaje y apoyo didáctico tanto para los
+      docentes como los alumnos.
+    </p>
+    <div class="justify-content-center text-center">
+      <p><a class="btn-warning btn font-bold" target="_blank" href="https://bit.ly/3ZrhDeQ">
+          Reglamento de Laboratorio</a></p>
+    </div>
+
+    <!-- Galeria de imagenes -->
+    <div class="row mb-4 px-2 g-0">
+      <div class="col-12">
+        <div class="galleryContainer bg-dark">
+          <div class="row g-0">
+            <!-- <div class="col-md-4 px-2">
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage1">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-1.jpg" alt="">
+              </a>
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage2">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-2.jpg" alt="">
+              </a>
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage3">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-3.jpg" alt="">
+              </a>
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage4">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-4.jpg" alt="">
+              </a>
+            </div>
+
+            <div class="col-md-4 px-2">
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage5">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-5.jpg" alt="">
+              </a>
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage6">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-6.jpg" alt="">
+              </a>
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage7">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-7.jpg" alt="">
+              </a>
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage8">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-8.jpg" alt="">
+              </a>
+            </div>
+
+            <div class="col-md-4 px-2">
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage9">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-9.jpg" alt="">
+              </a>
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage10">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-10.jpg" alt="">
+              </a>
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage11">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-11.jpg" alt="">
+              </a>
+              <a href="#!" data-bs-toggle="modal" data-bs-target="#modalImage12">
+                <img class="img-fluid w-100 shadow-1-strong rounded mb-4" src="img/Galeria/img-12.jpg" alt="">
+              </a>
+            </div> -->
+
+          </div>
+        </div>
+      </div>
+    </div>
 </section>
-<!--Fin Instalaciones-->
+<!-- Fin Galeria de imagenes -->
+
 
 <!-- Modals de la galería-->
-<!-- Imagen 1 -->
-<div tabindex="-1" aria-labelledby="modalImage3" aria-hidden="true" class="modal fade" id="modalImage1">
+<div tabindex="-1" aria-labelledby="modalImage1" aria-hidden="true" class="modal fade" id="modalImage1">
   <div class="modal-dialog modal-lg modal-dialog-centered">
     <div class="modal-content bg-transparent text-white text-center">
-      <img src="img/IMG_3878.webp" alt="" />
+      <img src="img/Galeria/img-1.jpg" alt="">
+      <!-- <p>Logo del Instituto Tecnológico de México Veracruz</p> -->
     </div>
   </div>
 </div>
 
-<!-- Imagen 2 -->
-<div tabindex="-1" aria-labelledby="modalImage4" aria-hidden="true" class="modal fade" id="modalImage2">
+<div tabindex="-1" aria-labelledby="modalImage2" aria-hidden="true" class="modal fade" id="modalImage2">
   <div class="modal-dialog modal-lg modal-dialog-centered">
     <div class="modal-content bg-transparent text-white text-center">
-      <img src="img/IMG_1110.webp" alt="" />
+      <img src="img/Galeria/img-2.jpg" alt="">
+      <!-- <p>Estatua Venus</p> -->
     </div>
   </div>
 </div>
 
-<!-- Imagen 3 -->
-<div tabindex="-1" aria-labelledby="modalImage5" aria-hidden="true" class="modal fade" id="modalImage3">
+<div tabindex="-1" aria-labelledby="modalImage3" aria-hidden="true" class="modal fade" id="modalImage3">
   <div class="modal-dialog modal-lg modal-dialog-centered">
     <div class="modal-content bg-transparent text-white text-center">
-      <img src="img/IMG_3879.webp" alt="" />
+      <img src="img/Galeria/img-3.jpg" alt="">
+      <!-- <p>Letras del Instituto Tecnológico de México Veracruz</p> -->
     </div>
   </div>
 </div>
 
-<!-- Imagen 4 -->
-<div tabindex="-1" aria-labelledby="modalImage6" aria-hidden="true" class="modal fade" id="modalImage4">
+<div tabindex="-1" aria-labelledby="modalImage4" aria-hidden="true" class="modal fade" id="modalImage4">
   <div class="modal-dialog modal-lg modal-dialog-centered">
     <div class="modal-content bg-transparent text-white text-center">
-      <img src="img/IMG_0959.webp" alt="" />
+      <img src="img/Galeria/img-4.jpg" alt="">
+      <!-- <p>Áreas recreativas</p> -->
     </div>
   </div>
 </div>
-<!-- Fin Modals de la galería-->
 
-<!-- Laboratorio de cómputo intensivo -->
-<div class="bg-primary pt-5"></div>
-<section class="seccionInstalaciones bg-dark p-2">
-  <div class="row px-2 g-0">
-    <div class="col-lg-6 col-12 p-2 shadow-sm">
-      <div class="d-flex justify-content-center align-items-center w-100 h-100">
-        <img src="img/IMG_1713.webp" class="img-fluid rounded" alt="" />
-      </div>
-    </div>
-
-    <div class="col-lg-6 col-12 p-4">
-      <div class="d-flex justify-content-center align-items-center w-100 h-100">
-        <div class="row g-0">
-          <div class="col-12">
-            <h2 class="tituloSeccionOscura text-center font-bold m-3">
-              Laboratorio de Cómputo Intensivo
-            </h2>
-            <div class="separadorSeccionOscura"></div>
-            <h4 class="text-center text-light fw-bold fs-3">
-              Responsable: Abelardo Rodríguez León
-            </h4>
-          </div>
-          <div class="col-12">
-            <p class="text-light" style="text-align: justify">
-              Adicionalmente a las oficinas administrativas y laboratorios de
-              cómputo, la carrera de ingeniería en sistemas computacionales
-              cuenta con el apoyo de un laboratorio de servidores en dónde el
-              alumnado tendrá la oportunidad de interactuar con clústeres con
-              diferentes enfoques tecnológicos.
-            </p>
-          </div>
-          <div class="col-12">
-            <div class="bg-dark">
-              <ul class="m-3 text-warning font-bold" style="text-align: justify">
-                <li>Agave: Clúster de alto procesamiento.</li>
-                <li>Peyote: Clúster de visualización en 4K.</li>
-                <li>Saguar: Clúster de servicios de virtualización.</li>
-                <li>Nopal: Clúster de alto rendimiento.</li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-12">
-            <p class="text-light" style="text-align: justify">
-              El Dr. Abelardo Rodríguez León (responsable del laboratorio de
-              servidores) pone a disposición el equipo a aquellos alumnos que
-              deseen realizar proyectos, experimentos, investigaciones, realizar
-              servicio social o residencias profesionales dentro del
-              laboratorio.
-
-              <br />
-              <br />
-              Información de contacto: abelardo.rl@veracruz.tecnm.mx
-            </p>
-          </div>
-        </div>
-      </div>
+<div tabindex="-1" aria-labelledby="modalImage5" aria-hidden="true" class="modal fade" id="modalImage5">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content bg-transparent text-white text-center">
+      <img src="img/Galeria/img-5.jpg" alt="">
+      <!-- <p>Áreas recreativas</p> -->
     </div>
   </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage6" aria-hidden="true" class="modal fade" id="modalImage6">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content bg-transparent text-white text-center">
+      <img src="img/Galeria/img-6.jpg" alt="">
+      <!-- <p>Edificio E</p> -->
+    </div>
+  </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage7" aria-hidden="true" class="modal fade" id="modalImage7">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content bg-transparent text-white text-center">
+      <img src="img/Galeria/img-7.jpg" alt="">
+      <!-- <p>Clúster del Laboratorio de Cómputo Intensivo</p> -->
+    </div>
+  </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage8" aria-hidden="true" class="modal fade" id="modalImage8">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content bg-transparent text-white text-center">
+      <img src="img/Galeria/img-8.jpg" alt="">
+      <!-- <p>Laboratorio de Cómputo Intensivo</p> -->
+    </div>
+  </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage9" aria-hidden="true" class="modal fade" id="modalImage9">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content bg-transparent text-white text-center">
+      <img src="img/Galeria/img-9.jpg" alt="">
+      <!-- <p>Laboratorio de cómputo</p> -->
+    </div>
+  </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage10" aria-hidden="true" class="modal fade" id="modalImage10">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content bg-transparent text-white text-center">
+      <img src="img/Galeria/img-10.jpg" alt="">
+      <!-- <p>Edificio U: Sistemas</p> -->
+    </div>
+  </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage11" aria-hidden="true" class="modal fade" id="modalImage11">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content bg-transparent text-white text-center">
+      <img src="img/Galeria/img-11.jpg" alt="">
+      <!-- <p>Laboratorio de cómputo</p> -->
+    </div>
+  </div>
+</div>
+
+<div tabindex="-1" aria-labelledby="modalImage12" aria-hidden="true" class="modal fade" id="modalImage12">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content bg-transparent text-white text-center">
+      <img src="img/Galeria/img-12.jpg" alt="">
+      <!-- <p>Laboratorio de cómputo</p> -->
+    </div>
+  </div>
+</div>
 </section>
+<!-- Fin Modals de la galería-->
 
 <div class="bg-primary pt-5"></div>
 <div class="p-4 text-center">
@@ -329,5 +362,10 @@ $jefeDepartamento =  $administrativos->getJefeDepartamento();
   <p class="font-semibold">Estudiantes de la Generación 2018 - 2023</p>
   <p>Daniela Castro Rodriguez, Nancy Daniela Mendez Arpidez, Marco Gabriel Cortés Toledo,
     Yelitza Magali Rosas Jiménez, Eric Manuel Montalvo Cruz y Gabriel Escobar Medina</p>
+</div>
+
+<div class="p-1 text-center">
+  <p class="font-semibold">Agradecimientos</p>
+  <p>Fernando Martinez, Eric Manuel Montalvo Cruz e Ivan Agame Malpica </p>
 </div>
 <!-- Fin Laboratorio de cómputo intensivo -->
