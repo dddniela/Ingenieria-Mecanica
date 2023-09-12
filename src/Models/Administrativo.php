d069ade9f1392044c31eaa62406816532a4fc5a3
Daniela Castro <daniela.castro@phs.mx>
2023-09-12 12:29:03 -0600

    fix de tablas y valores

--- src/Models/Administrativo.php
+++ src/Models/Administrativo.php
@@ -45,13 +45,13 @@ class Administrativo
          tbl_puesto_administrativo.nombre as 'nombrePuesto', 
         tbl_programa_estudio.nombre as 'nombreCarrera' FROM tbl_administrativo AS tbl_administrativo
         INNER JOIN tbl_administrativo_programa AS tbl_administrativo_programa 
-        ON tbl_administrativo.administrativoId = tbl_administrativo_carrera.administrativoId 
+        ON tbl_administrativo.administrativoId = tbl_administrativo_programa.administrativoId 
         INNER JOIN tbl_puesto_administrativo AS tbl_puesto_administrativo 
         ON tbl_administrativo.puestoId = tbl_puesto_administrativo.puestoId 
         INNER JOIN tbl_programa_estudio AS tbl_programa_estudio 
-        ON tbl_administrativo_carrera.programaId = tbl_programa_estudio.programaId 
-        AND tbl_administrativo_carrera.status = 1 
-        AND tbl_administrativo_carrera.programaId = " . $GLOBALS['programaId'] . "
+        ON tbl_administrativo_programa.programaId = tbl_programa_estudio.programaId 
+        AND tbl_administrativo_programa.status = 1 
+        AND tbl_administrativo_programa.programaId = " . $GLOBALS['programaId'] . "
         AND tbl_administrativo.puestoId = 1;";
         $jefeDep = mysqli_query($this->connection, $sql);
         $jefeDep = $jefeDep->fetch_object();
@@ -66,15 +66,15 @@ class Administrativo
         tbl_administrativo.imagen,
         tbl_puesto_administrativo.nombre as 'nombrePuesto', 
         tbl_programa_estudio.nombre as 'nombreCarrera' FROM tbl_administrativo AS tbl_administrativo
-        INNER JOIN tbl_administrativo_carrera AS tbl_administrativo_carrera 
-        ON tbl_administrativo.administrativoId = tbl_administrativo_carrera.administrativoId 
+        INNER JOIN tbl_administrativo_programa AS tbl_administrativo_programa 
+        ON tbl_administrativo.administrativoId = tbl_administrativo_programa.administrativoId 
         INNER JOIN tbl_puesto_administrativo AS tbl_puesto_administrativo 
         ON tbl_administrativo.puestoId = tbl_puesto_administrativo.puestoId 
         INNER JOIN tbl_programa_estudio AS tbl_programa_estudio 
-        ON tbl_administrativo_carrera.programaId = tbl_programa_estudio.programaId 
-        AND tbl_administrativo_carrera.status = 1 
-        AND tbl_administrativo_carrera.programaId = " . $GLOBALS['programaId'] . "
-        AND tbl_administrativo.puestoId = 4;";
+        ON tbl_administrativo_programa.programaId = tbl_programa_estudio.programaId 
+        AND tbl_administrativo_programa.status = 1 
+        AND tbl_administrativo_programa.programaId = " . $GLOBALS['programaId'] . "
+        AND tbl_administrativo.puestoId = 5;";
         $coordinador = mysqli_query($this->connection, $sql);
         $coordinador = $coordinador->fetch_object();
         return $coordinador;
