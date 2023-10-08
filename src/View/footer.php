249130951f23a18eb1193d1369c7630300af4a38
Daniela Castro <daniela.castro@phs.mx>
2023-10-08 13:09:17 -0600

    error de footer

--- src/View/footer.php
+++ src/View/footer.php
@@ -105,12 +105,7 @@
         const elementsH4List = document.getElementsByTagName('h4');
         const elementsH5List = document.getElementsByTagName('h5');
         const elementsH6List = document.getElementsByTagName('h6');
-        /* Función de alto contraste */
-        $(".contraste").on('click', function(event) {
-            console.log('???');
-            $("body, row, .bg-light, .bg--patron-blanco, .area, .lightSection, .bg--gris, .container, .bg--grisOscuro, .bg--grisMedio , .bg--patron-grisOscuro, .bg--grisAzulado, .oe-one-column, .oe-two-col, .dropdown-menu").toggleClass("bg-dark");
-        });
-
+        
         function getElementFontSize(element){
         //getComputedStyle nos devuelve las propiedades css de cada párrafo(elemento)
         const elementFontSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
